9cd0f511fcd2bd38009a532b35aae62d5dcde8d5
Mikael Gramont <mgramont@gmail.com>
2021-11-27 13:33:49 +0100

    Added (crappy) board for scale, and contact shadows

---
+++ components/Board.tsx
@@ -0,0 +1,133 @@
+import {useGLTF} from "@react-three/drei";
+import {useRef} from "react";
+import * as THREE from "three";
+
+export default function Board(props: any) {
+
+    // This makes the board about 120cm long:
+    const scale = .52;
+    const x = 1;
+    const y = 0.125;
+    const z = 2;
+
+    const group = useRef();
+
+    // @ts-ignore Whatever!
+    const { nodes, materials } = useGLTF('/board.glb')
+    return (
+        <group ref={group} {...props} dispose={null}  position={new THREE.Vector3(x,y,z)}  scale={new THREE.Vector3(scale, scale, scale)} >
+            <mesh castShadow receiveShadow geometry={nodes.Board.geometry} material={materials.Deck}>
+                <group position={[-0.04, -0.02, 0.6]} rotation={[0, 0, -Math.PI]} scale={[-1, -1, -1]}>
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Plane.geometry}
+                        material={nodes.Plane.material}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Plane_1.geometry}
+                        material={nodes.Plane_1.material}
+                    />
+                </group>
+                <group position={[-0.04, -0.02, -0.6]}>
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Plane.geometry}
+                        material={nodes.Plane.material}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Plane_1.geometry}
+                        material={nodes.Plane_1.material}
+                    />
+                </group>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Left_truck_and_wheels.geometry}
+                    material={nodes.Left_truck_and_wheels.material}
+                    position={[0, -0.05, 1]}
+                    rotation={[-0.05, 0, 0]}>
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Cylinder.geometry}
+                        material={nodes.Cylinder.material}
+                        position={[0.34, 0, 0]}
+                        rotation={[0.05, 0, 0]}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Cylinder_001.geometry}
+                        material={nodes.Cylinder_001.material}
+                        position={[-0.34, 0, 0]}
+                        rotation={[-3.09, 0, -Math.PI]}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Plane_2.geometry}
+                        material={nodes.Plane_2.material}
+                        rotation={[-0.39, 0, 0]}
+                        scale={[0.51, 0.51, 0.51]}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Plane_001.geometry}
+                        material={nodes.Plane_001.material}
+                        position={[0, 0.06, -0.06]}
+                        rotation={[2.77, 0, 0]}
+                        scale={[0.51, 0.51, 0.51]}
+                    />
+                </mesh>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Right_truck_and_wheels.geometry}
+                    material={nodes.Right_truck_and_wheels.material}
+                    position={[0, -0.05, -1]}
+                    rotation={[-3.09, 0, -Math.PI]}>
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Back_Right_wheel.geometry}
+                        material={nodes.Back_Right_wheel.material}
+                        position={[-0.34, 0, 0]}
+                        rotation={[-3.09, 0, -Math.PI]}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Front_Right_wheel.geometry}
+                        material={nodes.Front_Right_wheel.material}
+                        position={[0.34, 0, 0]}
+                        rotation={[0.05, 0, 0]}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Bottom_truck.geometry}
+                        material={nodes.Bottom_truck.material}
+                        rotation={[-0.39, 0, 0]}
+                        scale={[0.51, 0.51, 0.51]}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.Top_truck.geometry}
+                        material={nodes.Top_truck.material}
+                        position={[0, 0.06, -0.06]}
+                        rotation={[2.77, 0, 0]}
+                        scale={[0.51, 0.51, 0.51]}
+                    />
+                </mesh>
+            </mesh>
+        </group>
+    )
+}
--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,19 +1,22 @@
 import {useMemo} from "react";
-import {OrbitControls} from '@react-three/drei'
+import {ContactShadows, OrbitControls, PerspectiveCamera, useGLTF} from '@react-three/drei'
 import * as THREE from "three";
 
+import Board from "./Board";
 import Side from "./Side";
 import Strut from "./Strut";
 import Surface from "./Surface";
 import config from "../lib/config";
 import {calculateSidePoints, calculateStrutProps, calculateSurfacePoints} from "../lib/kicker";
-
+import {PlaneGeometry} from "three";
 
 interface Props {
     params: KickerParams;
 }
 
 export default function Scene({ params: { angle, arc, length, radius, width } }: Props) {
+    useGLTF.preload('/board.glb')
+
     const sidePoints: Array<THREE.Vector2> = useMemo(
         () => calculateSidePoints(angle, radius, config),
         [angle, radius, config]);
@@ -27,14 +30,22 @@ export default function Scene({ params: { angle, arc, length, radius, width } }:
     return (
         <>
             <OrbitControls makeDefault />
+            <PerspectiveCamera makeDefault position={[length / 2, 1.7, 6]} />
+
+            <ContactShadows opacity={.5} position={[0, 0, 0]} width={10} height={10} far={20} rotation={[Math.PI / 2, 0, 0]} />
 
             <directionalLight position={new THREE.Vector3(300, 10, 300)} />
             <directionalLight position={new THREE.Vector3(-100, 200, -120)} />
 
-            <Side name="side-left" points={sidePoints} config={config} width={width} color="red" />
-            <Side name="side-right" points={sidePoints} config={config} width={width} color="green" left/>
-            <Surface name="surface" points={surfacePoints} config={config} width={width} radius={radius} angle={angle} color="orange"/>
-            {strutPropsList.map((strutProps) => <Strut {...strutProps} key={`${strutProps.name}-${strutProps.angle}`} />)}
+            <Board position={new THREE.Vector3(6,0,5)} rotation={new THREE.Euler(0, .75, 0)}/>
+
+            {/*<Board position={new THREE.Vector3(0,0,0)} rotation={new THREE.Euler(0, 0, 0)}/>*/}
+            <group name="kicker">
+                <Side name="side-left" points={sidePoints} config={config} width={width} color="red" />
+                <Side name="side-right" points={sidePoints} config={config} width={width} color="green" left/>
+                <Surface name="surface" points={surfacePoints} config={config} width={width} radius={radius} angle={angle} color="orange"/>
+                {strutPropsList.map((strutProps) => <Strut {...strutProps} key={`${strutProps.name}-${strutProps.angle}`} />)}
+            </group>
         </>
     );
 }
